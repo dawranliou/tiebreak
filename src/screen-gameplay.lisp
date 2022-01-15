6aa684b8b1a90d3961afa5a002122982e11559b8
Daw-Ran Liou <hi@dawranliou.com>
2022-01-15 13:02:26 -0800

    Add player swing state

--- src/screen-gameplay.lisp
+++ src/screen-gameplay.lisp
@@ -1,10 +1,12 @@
 (in-package #:tiebreak)
 
 (defvar *frame-counter* 0)
+(defvar *player-frame-counter* 0)
 (defvar *finish-screen* 0)
 
 (defvar *player-texture*)
-(defvar *player-frame* 0)
+(defvar *player-frame-row* 0)
+(defvar *player-frame-col* 0)
 (defvar *player-frame-rec*)
 
 (defvar *player-score* 0)
@@ -16,30 +18,43 @@
 (defvar +player-step-x+ 200)
 (defvar +player-step-y+ 150)
 
-(defvar *player-walking-p* nil)
+(defvar *player-state* :idle)
+
+(defvar +player-idle-frame-index+ 0)
+(defvar +player-swing-frame-index+ 1)
+
+(defvar *player-swing-phase*)
+
 (defvar *target-x* 0)
 (defvar *target-y* 0)
 (defvar *player-x* 0)
 (defvar *player-y* 0)
-(defvar *player-dx* 0)
-(defvar *player-dy* 0)
+(defvar *player-dx* 3)
+(defvar *player-dy* 3)
 (defvar *player-dir* +1)                ; +1 right; -1 left
 
 (defun init-gameplay-screen ()
+  (when *player-texture*
+    (unload-texture *player-texture*)
+    (setq *player-texture* nil))
   (let ((player-texture (load-texture (namestring (merge-pathnames "player.png" *assets-path*)))))
     (setq *frame-counter* 0
           *finish-screen* 0
-          *player-walking-p* nil
+          *player-frame-counter* 0
+          *player-state* :idle
+          *player-swing-phase* nil
           *target-x* 0
           *target-y* 0
-          *player-frame* 0
-          *player-frame-rec* (make-rectangle :x 0.0 :y 0.0
-                                             :width (/ (texture-width player-texture) 4)
-                                             :height (texture-height player-texture))
+          *player-frame-row* 0
+          *player-frame-col* 0
+          *player-frame-rec* (make-rectangle :x (* 64 *player-frame-col*)
+                                             :y (* 64 +player-idle-frame-index+)
+                                             :width 64
+                                             :height 64)
           *player-x* 100
           *player-y* 100
-          *player-dx* 0
-          *player-dy* 0
+          *player-dx* 3
+          *player-dy* 3
           *player-dir* +1
           *player-texture* player-texture)))
 
@@ -48,31 +63,83 @@
 
 (defun update-gameplay-screen ()
   (incf *frame-counter*)
-  (when (<= 10 *frame-counter*)
-    (setq *frame-counter* 0)
-    (incf *player-frame*)
-    (when (<= 4 *player-frame*)
-      (setq *player-frame* 0))
-    (setf (rectangle-x *player-frame-rec*) (* *player-frame*
-                                              (texture-width *player-texture*)
-                                              1/4)))
+  (when (<= 60 *frame-counter*)
+    (setq *frame-counter* 0))
+
+  (incf *player-frame-counter*)
+
+  ;; input
 
   (when (is-key-pressed +key-enter+)
     (setq *finish-screen* 1))
 
   (when (is-key-down +key-right+)
-    (incf *player-x* 3))
+    (incf *player-x* *player-dx*))
 
   (when (is-key-down +key-left+)
-    (decf *player-x* 3))
+    (decf *player-x* *player-dx*))
 
   (when (is-key-down +key-up+)
-    (decf *player-y* 3))
+    (decf *player-y* *player-dy*))
 
   (when (is-key-down +key-down+)
-    (incf *player-y* 3)))
+    (incf *player-y* *player-dy*))
+
+  (when (is-key-down +key-z+)
+    (setq *player-state* :swing
+          *player-swing-phase* :enter))
+
+  (case *player-state*
+    (:idle (when (<= 10 *player-frame-counter*)
+             (setq *player-frame-counter* 0)
+             (incf *player-frame-col*)
+             (when (<= 4 *player-frame-col*)
+               (setq *player-frame-col* 0))
+             (setf (rectangle-x *player-frame-rec*) (* *player-frame-col*
+                                                       (texture-width *player-texture*)
+                                                       1/4))))
+    (:swing (case *player-swing-phase*
+              (:enter (progn (setq *player-swing-phase* :unit-turn
+                                     *player-swing-p* t
+                                     *player-dx* 0
+                                     *player-dy* 0
+                                     *player-frame-row* +player-swing-frame-index+
+                                     *player-frame-col* 0)
+                               (setf (rectangle-y *player-frame-rec*) (* *player-frame-row* 64))
+                               (setf (rectangle-x *player-frame-rec*) 0)))
+               (:unit-turn (when (is-key-up +key-z+)
+                               (setq *player-swing-phase* :swing
+                                     *player-dx* 3
+                                     *player-dy* 3
+                                     *player-frame-col* 1
+                                     *player-frame-counter* 0)))
+               (:swing (progn (when (<= 8 *player-frame-counter*)
+                                (setq *player-frame-counter* 0
+                                      *player-dx* 1
+                                      *player-dy* 1)
+                                (incf *player-frame-col*)
+                                (when (<= 4 *player-frame-col*)
+                                  (setq *player-swing-phase* :exit
+                                        *player-frame-counter* 0
+                                        *player-frame-col* 0
+                                        *player-frame-row* 0)
+                                  (setf (rectangle-y *player-frame-rec*) (* *player-frame-row* 64))
+                                  (setq *player-swing-p* nil)))
+                                (setf (rectangle-x *player-frame-rec*) (* *player-frame-col* 64))))
+               (:exit
+                (setf (rectangle-y *player-frame-rec*) (* *player-frame-row* 64))
+                (setq *player-swing-phase* nil
+                      *player-state* :idle
+                      *player-dx* 3
+                      *player-dy* 3))))))
+
 
 (defun draw-player ()
+  ;;(draw-text (format nil "Global Frame ~S" *frame-counter*) 220 160 20 +raywhite+)
+  ;;(draw-text (format nil "Player Frame ~S" *player-frame-counter*) 220 180 20 +raywhite+)
+  (draw-text (format nil "State ~S" *player-state*) 220 200 20 +raywhite+)
+  (draw-text (format nil "Swing ~S" *player-swing-phase*) 220 220 20 +raywhite+)
+  (draw-text (format nil "Frame (~S, ~S)" *player-frame-row* *player-frame-col*) 220 240 20 +raywhite+)
   (draw-rectangle-lines *player-x* *player-y* +player-w+ +player-h+ +blue+)
   (draw-texture-rec *player-texture*
                     *player-frame-rec*
