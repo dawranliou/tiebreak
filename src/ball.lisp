51b8a167718427774980931c58d3dadf4f0d52e7
Daw-Ran Liou <hi@dawranliou.com>
2022-01-30 19:51:42 -0800

    Tweak ball physics

--- src/ball.lisp
+++ src/ball.lisp
@@ -1,6 +1,6 @@
 (in-package #:tiebreak)
 
-(defparameter +gravity+ -10)
+(defparameter +gravity+ -20)
 (defparameter +ball-r+ 2.0)
 
 (define-entity ball (loc velocity projection bound elastic drag shape size))
@@ -14,7 +14,7 @@
                  :bound/xmax 30.0
                  :bound/zmin -40.0
                  :bound/zmax 40.0
-                 :elastic/damp 0.9
+                 :elastic/damp 0.8
                  :drag/air 0.998
                  :velocity/x vx
                  :velocity/y 0.0
@@ -33,16 +33,21 @@
     (setf (loc/x b) (+ loc/x (* velocity/x dt))
           (loc/y b) (+ loc/y (* velocity/y dt))
           (loc/z b) (+ loc/z (* velocity/z dt))
-          (velocity/x b) (* velocity/x drag/air)
+          (velocity/x b) (if (< (abs velocity/x) 5)
+                             0.0
+                             (* velocity/x drag/air))
           (velocity/y b) (+ velocity/y (* +gravity+ dt))
-          (velocity/z b) (* velocity/z drag/air))
+          (velocity/z b) (if (< (abs velocity/z) 5)
+                             0.0
+                             (* velocity/z drag/air)))
     (when (< (loc/y b) (/ size/h 2))    ; Hit the ground?
       (setf (loc/y b) (/ size/h 2)
-            (velocity/y b) (* velocity/y -1 elastic/damp)))))
+            (velocity/y b) (if (< (abs velocity/y) 4)
+                               0
+                               (* velocity/y -1 elastic/damp))))))
 
 
 (defun ball-hit (b)
-  (setf (loc/y b) 2.0
-        (velocity/x b) -30
+  (setf (velocity/x b) -30
         (velocity/y b) 10
         (velocity/z b) -30))
